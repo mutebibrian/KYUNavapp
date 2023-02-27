faf0b0f93573b97086b3c338b93e6a2daadf0b9f
BRIAN MUTEBI <33450898+mutebibrian@users.noreply.github.com>
2023-02-27 14:08:19 +0300

    Initial commit

---
+++ app/src/main/java/com/intellectsoft/navapp/CareerFragment.kt
@@ -0,0 +1,21 @@
+package com.intellectsoft.navapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CareerFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        //Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_career,container,false)
+
+
+
+    }
+}
